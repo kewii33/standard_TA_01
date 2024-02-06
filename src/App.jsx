0477c16194e26cfdeb15659bb6f10198cfcee8ca
kewii33 <kewiii071@gmail.com>
2024-02-06 14:40:35 +0900

    ex1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import "./App.css";
+import { useDispatch, useSelector } from 'react-redux';
+import './App.css';
+import { plus, minus } from './redux/modules/calculator';
+import { useState } from 'react';
 
 function App() {
+  const globalNumber = useSelector((state) => state.calculator.globalNumber);
+  const dispatch = useDispatch();
+  const [number, setNumber] = useState(0);
+
+  const onChangeHandler = (event) => {
+    const { value } = event.target;
+    setNumber(+value);
+  };
+
+  const onClickPlusButtonHandler = () => {
+    dispatch(plus(number));
+  };
+
+  const onClickMinusButtonHandler = () => {
+    dispatch(minus(number));
+  };
+
   return (
     <div className="App">
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+        <input type="number" onChange={onChangeHandler} /> 만큼을{' '}
+        <button onClick={onClickPlusButtonHandler}>더할게요</button>{' '}
+        <button onClick={onClickMinusButtonHandler}>뺄게요</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{globalNumber}</p>
       </div>
     </div>
   );
--- src/redux/modules/calculator.js
+++ src/redux/modules/calculator.js
@@ -1,10 +1,40 @@
+import { type } from '@testing-library/user-event/dist/type';
+
 // 초기 상태값
 const initialState = {
-  number: 0,
+  globalNumber: 0,
 };
 
+const PLUS = 'PLUS';
+const MINUS = 'MINUS';
+
+export const plus = (payload) => ({
+  type: PLUS,
+  payload: payload,
+});
+
+export const minus = (payload) => ({
+  type: MINUS,
+  payload: payload,
+});
+
 // 리듀서
-const calculator = (state = initialState, action) => {};
+const calculator = (state = initialState, action) => {
+  switch (action.type) {
+    case PLUS:
+      return {
+        ...state,
+        globalNumber: state.globalNumber + action.payload,
+      };
+    case MINUS:
+      return {
+        ...state,
+        globalNumber: state.globalNumber - action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 // 모듈파일에서는 리듀서를 export default 한다.
 export default calculator;
